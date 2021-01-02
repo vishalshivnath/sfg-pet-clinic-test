139a6c13745ef19ffb71505c4756576c689151df
vishalshivnath <vishalshivnath@gmail.com>
2021-01-02 15:15:02 +0530

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinictest/SfgPetClinicTestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SfgPetClinicTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinictest/SfgPetClinicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
