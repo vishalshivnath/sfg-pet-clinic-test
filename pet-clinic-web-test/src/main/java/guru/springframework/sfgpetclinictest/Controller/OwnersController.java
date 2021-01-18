96cf7020a04223055ac15174e400a32d039ec9d4
vishalshivnath <vishalshivnath@gmail.com>
2021-01-18 23:30:26 +0530

    Project Pet Clinic Web Test

--- pet-clinic-web-test/src/main/java/guru/springframework/sfgpetclinictest/Controller/OwnersController.java
+++ pet-clinic-web-test/src/main/java/guru/springframework/sfgpetclinictest/Controller/OwnersController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpetclinictest.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinictest.services.OwnerService;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnersController {
+
+  private final OwnerService ownerService;
+
+  public OwnersController(final OwnerService ownerService) {
+    this.ownerService = ownerService;
+  }
+
   @RequestMapping({"","/","/index.html","/index"})
-  public String listVets(){
+  public String listVets(Model model){
+    model.addAttribute("owners",ownerService.findAll());
     return "owners/index";
   }
+
 }
