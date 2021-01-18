bb29f86036ed340cc92c320316da9ffe1ea3ea98
vishalshivnath <vishalshivnath@gmail.com>
2021-01-18 23:43:45 +0530

    Project Pet Clinic Web Test

--- pet-clinic-web-test/src/main/java/guru/springframework/sfgpetclinictest/Controller/VetController.java
+++ pet-clinic-web-test/src/main/java/guru/springframework/sfgpetclinictest/Controller/VetController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinictest.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinictest.services.VetService;
 
 @Controller
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(final VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @RequestMapping({"/vets","/vets/index.html","/vets/index"})
-  public String listVets(){
+  public String listVets(Model model){
+    model.addAttribute("vets",vetService.findAll());
     return "vets/index";
   }
 }
